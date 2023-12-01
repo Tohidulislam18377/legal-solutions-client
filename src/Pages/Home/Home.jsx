3d7d344685d9df546427bbe2b2334d25bbebf431
MD.TOHIDUL ISLAM <tohidulislam1837@gmail.com>
2023-12-01 23:18:23 +0600

    added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,8 +13,10 @@ import img10 from '../../assets/images/address.png'
 import img11 from '../../assets/images/call.png'
 import img12 from '../../assets/images/email.png'
 import img13 from '../../assets/images/time.png'
+import img14 from '../../assets/images/logo.png'
 
 import { Carousel, Typography } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 
 
 
@@ -202,53 +204,188 @@ const Home = () => {
                 <p style={{ fontSize: '16px', fontWeightL: '400' }}>There are many variations of passages of Lorem Ipsum available, but the majority <br /> have suffered alteration in some form, by injected humour.</p>
             </div>
             {/* main */}
-            <div style={{display:'flex', columnGap:'30px', marginBottom:'30px'}}>
+            <div style={{ display: 'flex', columnGap: '30px', marginBottom: '30px' }}>
 
                 <div className="contact">
-                    <div style={{height:'285px', width:'285px', backgroundColor:'var(--Dark-07, #F3F3F3)', borderRight:'1px solid #CFCFCF', borderBottom:'1px solid #CFCFCF',textAlign:'center', borderRadius:'5px 0px 0px 0px'}}>
-                        <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', paddingTop:'25%'}}>
-                        <img style={{width:'40px', height:'40px'}} src={img10} alt="" />
-                        <p style={{fontSize:'24px', fontWeight:'800'}}>Address</p>
-                        <p style={{fontSize:'16px', fontWeight:'400'}}>A108 Adam Street, <br />
-                            New York, NY 535022</p>
+                    <div style={{ height: '285px', width: '285px', backgroundColor: 'var(--Dark-07, #F3F3F3)', borderRight: '1px solid #CFCFCF', borderBottom: '1px solid #CFCFCF', textAlign: 'center', borderRadius: '5px 0px 0px 0px' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: '25%' }}>
+                            <img style={{ width: '40px', height: '40px' }} src={img10} alt="" />
+                            <p style={{ fontSize: '24px', fontWeight: '800' }}>Address</p>
+                            <p style={{ fontSize: '16px', fontWeight: '400' }}>A108 Adam Street, <br />
+                                New York, NY 535022</p>
                         </div>
                     </div>
-                    <div style={{height:'285px', width:'285px', backgroundColor:'var(--Dark-07, #F3F3F3)',borderBottom:'1px solid #CFCFCF', borderRadius:'0px 5px 0px 0px'}}>
-                        <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', paddingTop:'25%'}}>
-                        <img style={{width:'40px', height:'40px'}} src={img11} alt="" />
-                        <p style={{fontSize:'24px', fontWeight:'800'}}>Call Us</p>
-                        <p style={{fontSize:'16px', fontWeight:'400'}}>+88 01750 00 00 00 <br />
-                            +88 01750 00 00 00</p>
+                    <div style={{ height: '285px', width: '285px', backgroundColor: 'var(--Dark-07, #F3F3F3)', borderBottom: '1px solid #CFCFCF', borderRadius: '0px 5px 0px 0px' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: '25%' }}>
+                            <img style={{ width: '40px', height: '40px' }} src={img11} alt="" />
+                            <p style={{ fontSize: '24px', fontWeight: '800' }}>Call Us</p>
+                            <p style={{ fontSize: '16px', fontWeight: '400' }}>+88 01750 00 00 00 <br />
+                                +88 01750 00 00 00</p>
                         </div>
                     </div>
-                    <div style={{height:'285px', width:'285px', backgroundColor:'var(--Dark-07, #F3F3F3)',borderRight:'1px solid #CFCFCF',borderRadius:'0px 0px 0px 5px'}}>
-                        <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', paddingTop:'25%'}}>
-                        <img style={{width:'40px', height:'40px'}}  src={img12} alt="" />
-                        <p>Email Us</p>
-                        <p>info@gmail.com <br />
-                            info@gmail.com</p>
+                    <div style={{ height: '285px', width: '285px', backgroundColor: 'var(--Dark-07, #F3F3F3)', borderRight: '1px solid #CFCFCF', borderRadius: '0px 0px 0px 5px' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: '25%' }}>
+                            <img style={{ width: '40px', height: '40px' }} src={img12} alt="" />
+                            <p style={{ fontSize: '24px', fontWeight: '800' }}>Email Us</p>
+                            <p style={{ fontSize: '16px', fontWeight: '400' }}>info@gmail.com <br />
+                                info@gmail.com</p>
                         </div>
                     </div>
-                    <div style={{height:'285px', width:'285px', backgroundColor:'var(--Dark-07, #F3F3F3)',borderRadius:'0px 0px 5px 0px'}}>
-                       <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', paddingTop:'25%'}}>
-                       <img src={img13} alt="" />
-                        <p style={{fontSize:'24px', fontWeight:'800'}}>Working Hours</p>
-                        <p style={{fontSize:'16px', fontWeight:'400'}}>Mon-Fri: 9AM to 5PM <br />
-                            Sunday: 9AM to 1 PM</p>
-                       </div>
+                    <div style={{ height: '285px', width: '285px', backgroundColor: 'var(--Dark-07, #F3F3F3)', borderRadius: '0px 0px 5px 0px' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: '25%' }}>
+                            <img src={img13} alt="" />
+                            <p style={{ fontSize: '24px', fontWeight: '800' }}>Working Hours</p>
+                            <p style={{ fontSize: '16px', fontWeight: '400' }}>Mon-Fri: 9AM to 5PM <br />
+                                Sunday: 9AM to 1 PM</p>
+                        </div>
                     </div>
                 </div>
 
-                    <div style={{marginRight:'20px'}}>
-                      <input className="message" style={{display:'block', width:'350px', height:'72px', backgroundColor:'var(--Dark-07, #F3F3F3)', borderRadius:'8px', paddingLeft:'20px', marginBottom:'20px', outlineStyle:'none'}} type="text" name="" placeholder="Your Name" id="" />
-                      <input className="message" style={{display:'block',width:'350px', height:'72px', backgroundColor:'var(--Dark-07, #F3F3F3)', borderRadius:'8px', paddingLeft:'20px', marginBottom:'20px', outlineStyle:'none'}} type="email" name="" placeholder="You Email" id="" />
-                      <input className="message" style={{display:'block',width:'350px', height:'72px', backgroundColor:'var(--Dark-07, #F3F3F3)', borderRadius:'8px', paddingLeft:'20px', marginBottom:'20px', outlineStyle:'none'}} type="text" name="" placeholder="Subject" id="" />
-                      <input className="message" style={{display:'block',width:'350px', height:'200px', backgroundColor:'var(--Dark-07, #F3F3F3)', borderRadius:'8px', paddingLeft:'20px', marginBottom:'20px', outlineStyle:'none',}} type="text" name="" placeholder="Message" id="" />
-                        <button style={{backgroundColor:'#B68C5A', width:"350px", height:'72px', borderRadius:'8px'}}>Send Message</button>
-                    </div>
+                <div style={{ marginRight: '20px' }}>
+                    <input className="message"
+                        style={{
+                            display: 'block',
+                            width: '350px',
+                            height: '72px',
+                            backgroundColor: 'var(--Dark-07, #F3F3F3)',
+                            borderRadius: '8px',
+                            paddingLeft: '20px',
+                            marginBottom: '20px',
+                            outlineStyle: 'none'
+                        }}
+                        type="text" name="" placeholder="Your Name" id="" />
+                    <input className="message"
+                        style={{
+                            display: 'block',
+                            width: '350px',
+                            height: '72px',
+                            backgroundColor: 'var(--Dark-07, #F3F3F3)',
+                            borderRadius: '8px',
+                            paddingLeft: '20px',
+                            marginBottom: '20px',
+                            outlineStyle: 'none'
+                        }}
+                        type="email" name="" placeholder="You Email" id="" />
+                    <input className="message"
+                        style={{
+                            display: 'block',
+                            width: '350px',
+                            height: '72px',
+                            backgroundColor: 'var(--Dark-07, #F3F3F3)',
+                            borderRadius: '8px',
+                            paddingLeft: '20px',
+                            marginBottom: '20px',
+                            outlineStyle: 'none'
+                        }}
+                        type="text" name="" placeholder="Subject" id="" />
+                    <input className="message"
+                        style={{
+                            display: 'block',
+                            width: '350px',
+                            height: '200px',
+                            backgroundColor: 'var(--Dark-07, #F3F3F3)',
+                            borderRadius: '8px',
+                            paddingLeft: '20px',
+                            marginBottom: '20px',
+                            outlineStyle: 'none',
+                        }}
+                        type="text" name="" placeholder="Message" id="" />
+                    <button
+                        style={{
+                            backgroundColor: '#B68C5A',
+                            width: "350px",
+                            height: '72px',
+                            borderRadius: '8px'
+                        }}
+                    >Send Message</button>
+                </div>
 
             </div>
+            {/* footer section */}
+            {/* main */}
+            <div className="ms-5 me-5 mb-5 rounded"
+                style={{
 
+                    display: 'flex',
+                    columnGap: '10px',
+                    backgroundColor: ' var(--Dark-01, #111)',
+                    height: '400px',
+                    // width: '1920px'
+                }}>
+                <div
+                    style={
+                        {
+                            paddingTop: '80px',
+                            paddingLeft: '30px',
+                            paddingRight: '30px',
+                            color: 'white',
+                            fontSize: '16px',
+                            fontWeight: '400'
+                        }} >
+                    <img className="mb-5" src={img14} alt="" />
+                    <p
+                        style={{
+                            width: '259px',
+                            textAlign: 'left',
+
+                        }}
+                    >There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
+                </div>
+                <div
+                    style={{
+                        paddingTop: '90px',
+                        width: '50%',
+                        color: 'white',
+                    }}>
+                    <p style={{ fontSize: '24px', fontWeight: '800', paddingBottom: '25px' }}>Usefull Link</p>
+                    <div style={{
+                        fontSize: '16px',
+                        fontWeight: '400',
+                        paddingLeft:'30px'
+                    }}>
+                       <Link to="/"><p>Homes</p></Link>
+                       <Link to="/"><p>About Us</p></Link>
+                        <Link to="/"><p>Cases</p></Link>
+                        <Link to="/"><p>Blog</p></Link>
+                        <Link to="/"><p>Contact Us</p></Link>
+                       
+                       
+                    </div>
+                </div>
+
+                <div style={{ width:'55%', paddingTop: '90px',color:'white'}}>
+                    <p  style={{ fontSize: '24px', fontWeight: '800', paddingBottom: '25px' }}>Contact Now</p>
+                    <div style={{
+                        fontSize: '16px',
+                        fontWeight: '400',
+                    }}>
+                    <p>555 4th 5t NW, Washington
+                        DC 20530, United States</p>
+                    <p>+88 01750 000 000</p>
+                    <p>+88 01750 000 000</p>
+                    <p>info@gmail.com</p>
+                    </div>
+                    <p>info@gmail.com</p>
+                </div>
+                <div style={{ width:'50%', paddingTop: '90px',color:'white'}}>
+                    <p style={{ fontSize: '24px', fontWeight: '800', paddingBottom: '25px',paddingLeft:'20px' }}>Subscribe</p>
+                    <p style={{ width: '217px', paddingBottom:'20px', paddingLeft:'20px' }}>Subscribe for our latest & Articles. We Won’t Give You Spam Mails</p>
+                    <div style={{ display: 'flex', paddingRight: '20px', paddingLeft:'20px' }}>
+                        <input style={{outlineStyle:'none', backgroundColor:"white", color:'black', paddingLeft:'10px', borderRadius:'5px 0px 0px 5px' }} type="email" name="" className="main" placeholder="Email" id="" />
+                        <button style={{ backgroundColor: '#B68C5A', borderRadius:'0px 5px 5px 0px' }}><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
+                            <g clip-path="url(#clip0_12_37)">
+                                <path d="M12.7279 21.2427L4.52197 16.8989C10.4846 13.6228 16.9629 11.3888 23.6775 10.2931C22.5826 17.0078 20.3492 23.4863 17.0738 29.4494L12.7279 21.2427ZM12.7279 21.2427L18.0312 15.9394" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_12_37">
+                                    <rect width="24" height="24" fill="white" transform="translate(0 17) rotate(-45)" />
+                                </clipPath>
+                            </defs>
+                        </svg></button>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
